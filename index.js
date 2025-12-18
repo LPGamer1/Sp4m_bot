d8723dd769733be3200eb06c93e31bd46513b530
scripter <lucaspiske0@gmail.com>
2025-12-18 12:31:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 const http = require('http');
 const startBot = require('./core.js');
 
-// Servidor HTTP para o Render
 http.createServer((req, res) => {
-  res.write("Sistema Multi-Bot Online (2 Instancias Ativas)");
+  res.write(`SP4M_B0T Ativo - Modo: ${process.env.BOT_TYPE || 'MAIN'}`);
   res.end();
 }).listen(process.env.PORT || 3000);
 
-console.log("🚀 Iniciando Bots...");
-
-// Inicia Instância 1
 startBot(process.env.TOKEN_1, process.env.CLIENT_ID_1);
-
-// Inicia Instância 2
 startBot(process.env.TOKEN_2, process.env.CLIENT_ID_2);
